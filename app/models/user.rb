c791dd5591a0ddc199575919c3dffbe4130faceb
Sebastian Kopp <smkopp92@gmail.com>
2018-03-08 19:21:00 -0500

    update rspec user sign up test for matching passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ApplicationRecord
 
   validates_presence_of :username
   validates_uniqueness_of :username
-
-  validates_confirmation_of :password
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -17,6 +17,14 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     }
   }
 
+  let(:mismatched_passwords) {
+    {
+      username: "sebdawg",
+      password: "awesome password",
+      password_confirmation: "unawesome password"
+    }
+  }
+
   describe "POST #create" do
     context "with valid params" do
       it "creates a new User" do
@@ -27,14 +35,21 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
       it "returns the username in json form" do
         post :create, params: { user: valid_attributes }
-        expect(response.body).to eq "sebdawg"
+        expect(response.body).to eq "{\"user\":\"sebdawg\",\"status\":200}"
       end
     end
 
     context "with invalid params" do
       it "returns a success response (i.e. to display the 'new' template)" do
         post :create, params: { user: invalid_attributes }
-        expect(response.body).to eq "[\"Password can't be blank\",\"Username can't be blank\"]"
+        expect(response.body).to eq "{\"errors\":[\"Password can't be blank\",\"Username can't be blank\"],\"status\":401}"
+      end
+    end
+
+    context "with mismatched password confirmation" do
+      it "returns a success response (i.e. to display the 'new' template)" do
+        post :create, params: { user: mismatched_passwords }
+        expect(response.body).to eq "{\"errors\":[\"Password confirmation doesn't match Password\"],\"status\":401}"
       end
     end
   end
